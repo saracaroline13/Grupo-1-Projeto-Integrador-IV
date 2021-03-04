5c4f84ac28b6ce726fcb258557f018775161d8f7
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-03-04 20:09:53 -0300

    Teste

--- Front-end/Optimus/src/app/app.module.ts
+++ Front-end/Optimus/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ProdutoComponent } from './produto/produto.component';
+import { TabelaTesteComponent } from './tabela-teste/tabela-teste.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    ProdutoComponent
+    ProdutoComponent,
+    TabelaTesteComponent
   ],
   imports: [
     BrowserModule
---
+++ Front-end/Optimus/src/app/tabela-teste/tabela-teste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-teste',
+  templateUrl: './tabela-teste.component.html',
+  styleUrls: ['./tabela-teste.component.css']
+})
+export class TabelaTesteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
