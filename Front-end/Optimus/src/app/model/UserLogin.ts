725f2e3fc4aa6f095363ffe952618266bce43c0d
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-03-31 19:21:37 -0300

    Service para autenticãção do usuario

---
+++ Front-end/Optimus/src/app/model/UserLogin.ts
@@ -0,0 +1,8 @@
+export class UserLogin
+{
+  public id_usuario: number
+  public email : string
+  public nome: string
+  public senha: string
+  public token: string
+}
---
+++ Front-end/Optimus/src/app/model/Usuario.ts
@@ -0,0 +1,10 @@
+export class Usuario{
+  public id_usuario: number
+  public nome: string
+  public sobrenome: string
+  public cpf: number
+  public telefone: number
+  public rg: number
+  public email: string
+  public senha: string
+}
