14c24cdd4c1421e812510ae0581bf45bcb286f75
gabzoom <gabriel.rodriguesxs@gmail.com>
2021-03-31 17:52:29 -0300

    CRUD Cliente

---
+++ Back-end/optimus/src/main/java/com/optimus/model/Usuario.java
@@ -0,0 +1,102 @@
+package com.optimus.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name= "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue (strategy= GenerationType.IDENTITY)
+	private long id_cliente;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private String sobrenome;
+	
+	@Column
+	private int cpf;
+	
+	@Column
+	private int telefone;
+	
+	@Column
+	private int rg;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String senha;
+
+	public long getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(long id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public int getRg() {
+		return rg;
+	}
+
+	public void setRg(int rg) {
+		this.rg = rg;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
---
+++ Back-end/optimus/src/main/java/com/optimus/repository/ClienteRepository.java
@@ -0,0 +1,17 @@
+package com.optimus.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.optimus.model.Usuario;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Usuario, Long> {
+
+	public List<Usuario> findAllByEmailContainingIgnoreCase(String email);
+
+	public Optional<Usuario> findByEmail(String email);
+}
