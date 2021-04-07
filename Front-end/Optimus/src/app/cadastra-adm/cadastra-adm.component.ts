e0d9ffe0787a1746a7d894688d50e9f16dce2660
LucasSG00 <lucas_s_goncalves@hotmail.com>
2021-04-06 23:04:30 -0300

    Validação do cadastrar

--- Front-end/Optimus/src/app/cadastra-adm/cadastra-adm.component.ts
+++ Front-end/Optimus/src/app/cadastra-adm/cadastra-adm.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { UsuarioService } from '../service/usuario.service';
 
 @Component({
   selector: 'app-cadastra-adm',
@@ -7,9 +10,207 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastraADMComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario()
 
-  ngOnInit(): void {
+  confirmSenha: string;
+
+  nome: string;
+  sobrenome: string;
+  rg: string;
+  cpf: string;
+  telefone: string;
+  rua: string;
+  numero: string;
+  bairro: string;
+  cep: string;
+  cidade: string;
+  salario: string;
+  email: string;
+  emailEmpresarial: string;
+  nascimento: string;
+
+  nomeOk: boolean = false;
+  sobrenomeOk: boolean = false;
+  rgOk: boolean = false;
+  cpfOk: boolean = false;
+  telefoneOk: boolean = false;
+  ruaOk: boolean = false;
+  numeroOk: boolean = false;
+  bairroOk: boolean = false;
+  cepOk: boolean = false;
+  cidadeOk: boolean = false;
+  salarioOk: boolean = false;
+  emailOk: boolean = false;
+  emailEmpresarialOk: boolean = false;
+  nascimentoOk: boolean = false;
+
+  alertaNome: string;
+  alertaSobrenome: string;
+  alertaRg: string;
+  alertaCpf: string;
+  alertaTelefone: string;
+  alertaRua: string;
+  alertaNumero: string;
+  alertaBairro: string;
+  alertaCep: string;
+  alertaCidade: string;
+  alertaSalario: string;
+  alertaEmail: string;
+  alertaEmailEmpresarial: string;
+  alertaNascimento: string;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  confirmarSenha(event: any) {
+    this.confirmSenha = event.target.value;
+  }
+
+  validaNome() {
+    if (this.nome.length < 3) {
+      this.nomeOk = false;
+      this.alertaNome = 'nome inválido';
+      console.log("entrei")
+    } else {
+      this.nomeOk = true;
+      this.alertaNome = '';
+      console.log("sei la")
+    }
+  }
+
+  validaSobrenome() {
+    if (this.sobrenome.length < 3) {
+      this.sobrenomeOk = false;
+      this.alertaSobrenome = 'sobrenome inválido';
+    } else {
+      this.sobrenomeOk = true;
+      this.alertaSobrenome = '';
+    }
+  }
+
+  validaRg() {
+    if (this.rg.length < 9 || this.rg.length > 9) {
+      this.rgOk = false;
+      this.alertaRg = 'rg inválido';
+    } else {
+      this.rgOk = true;
+      this.alertaRg = '';
+    }
+  }
+
+  validaCpf() {
+    if (this.cpf.length < 11 || this.cpf.length > 11) {
+      this.cpfOk = false;
+      this.alertaCpf = 'CPF inválido';
+    } else {
+      this.cpfOk = true;
+      this.alertaCpf = '';
+    }
+  }
+
+  validaTelefone() {
+    if (this.telefone.length < 11 || this.telefone.length > 11) {
+      this.telefoneOk = false;
+      this.alertaTelefone = 'telefone inválido';
+    } else {
+      this.telefoneOk = true;
+      this.alertaTelefone = '';
+    }
+  }
+
+  validaRua() {
+    if (this.rua.length < 3) {
+      this.ruaOk = false;
+      this.alertaRua = 'rua inválido';
+    } else {
+      this.ruaOk = true;
+      this.alertaRua = '';
+    }
+  }
+
+  validaNumero() {
+    if (this.numero.length < 1 || this.numero.length > 4) {
+      this.numeroOk = false;
+      this.alertaNumero = 'numero inválido';
+    } else {
+      this.numeroOk = true;
+      this.alertaNumero = '';
+    }
+  }
+
+  validaBairro() {
+    if (this.bairro.length < 3) {
+      this.bairroOk = false;
+      this.alertaBairro = 'bairro inválido';
+    } else {
+      this.bairroOk = true;
+      this.alertaBairro = '';
+    }
+  }
+
+  validaCep() {
+    if (this.cep.length < 8 || this.cep.length > 8) {
+      this.cepOk = false;
+      this.alertaCep = 'cep inválido';
+    } else {
+      this.cepOk = true;
+      this.alertaCep = '';
+    }
+  }
+
+  validaCidade() {
+    if (this.cidade.length < 3) {
+      this.cidadeOk = false;
+      this.alertaCidade = 'cidade inválido';
+    } else {
+      this.cidadeOk = true;
+      this.alertaCidade = '';
+    }
+  }
+
+  validaEmail() {
+    if (this.email.length < 3) {
+      this.emailOk = false;
+      this.alertaEmail = 'email inválido';
+    } else {
+      this.emailOk = true;
+      this.alertaEmail = '';
+    }
+  }
+
+  validaEmailEmpresarial() {
+    if (this.emailEmpresarial.length < 3) {
+      this.emailEmpresarialOk = false;
+      this.alertaEmailEmpresarial = 'Email empresarial inválido';
+    } else {
+      this.emailEmpresarialOk = true;
+      this.alertaEmailEmpresarial = '';
+    }
+  }
+
+  validaNascimento() {
+    if (this.nascimento.length < 8 || this.nascimento.length > 8) {
+      this.nascimentoOk = false;
+      this.alertaNascimento = 'nascimento inválido';
+    } else {
+      this.nascimentoOk = true;
+      this.alertaNascimento = '';
+    }
+  }
+
+  validaSalario() {
+    if (this.salario.length < 1) {
+      this.salarioOk = false;
+      this.alertaSalario = 'salario inválido';
+    } else {
+      this.salarioOk = true;
+      this.alertaSalario = '';
+    }
   }
 
 }
--- Front-end/Optimus/src/app/cadastra/cadastra.component.ts
+++ Front-end/Optimus/src/app/cadastra/cadastra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+import { UsuarioService } from '../service/usuario.service';
 
 @Component({
   selector: 'app-cadastra',
@@ -7,9 +9,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastraComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario();
 
-  ngOnInit(): void {
+  confirmSenha: string;
+  
+  nome: string;
+  sobrenome: string;
+  rg: string;
+  cpf: string;
+  telefone: string;
+  email: string;
+
+  nomeOk: boolean = false;
+  sobrenomeOk: boolean = false;
+  rgOk: boolean = false;
+  cpfOk: boolean = false;
+  telefoneOk: boolean = false;
+  emailOk: boolean = false;
+
+  alertaNome: string;
+  alertaSobrenome: string;
+  alertaRg: string;
+  alertaCpf: string;
+  alertaTelefone: string;
+  alertaEmail: string;
+  constructor(
+    private usuarioService: UsuarioService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  confirmarSenha(event: any) {
+    this.confirmSenha = event.target.value;
+  }
+
+  validaNome() {
+    if (this.nome.length < 3) {
+      this.nomeOk = false;
+      this.alertaNome = 'nome inválido';
+      console.log("entrei")
+    } else {
+      this.nomeOk = true;
+      this.alertaNome = '';
+      console.log("sei la")
+    }
+  }
+
+  validaSobrenome() {
+    if (this.sobrenome.length < 3) {
+      this.sobrenomeOk = false;
+      this.alertaSobrenome = 'sobrenome inválido';
+    } else {
+      this.sobrenomeOk = true;
+      this.alertaSobrenome = '';
+    }
+  }
+
+  validaRg() {
+    if (this.rg.length < 9 || this.rg.length > 9) {
+      this.rgOk = false;
+      this.alertaRg = 'rg inválido';
+    } else {
+      this.rgOk = true;
+      this.alertaRg = '';
+    }
+  }
+
+  validaCpf() {
+    if (this.cpf.length < 11 || this.cpf.length > 11) {
+      this.cpfOk = false;
+      this.alertaCpf = 'CPF inválido';
+    } else {
+      this.cpfOk = true;
+      this.alertaCpf = '';
+    }
+  }
+
+  validaTelefone() {
+    if (this.telefone.length < 11 || this.telefone.length > 11) {
+      this.telefoneOk = false;
+      this.alertaTelefone = 'telefone inválido';
+    } else {
+      this.telefoneOk = true;
+      this.alertaTelefone = '';
+    }
+  }
+
+  validaEmail() {
+    if (this.email.length < 3) {
+      this.emailOk = false;
+      this.alertaEmail = 'email inválido';
+    } else {
+      this.emailOk = true;
+      this.alertaEmail = '';
+    }
   }
 
 }
