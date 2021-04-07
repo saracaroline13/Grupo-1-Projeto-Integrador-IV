636841c1931fc5858d95adaf04690a0f32572606
MatheusSCaetano <matheuscaetano260401@gmail.com>
2021-04-06 22:27:05 -0300

    Consumo Login Cadastrar

--- Front-end/Optimus/src/app/cadastra-adm/cadastra-adm.component.ts
+++ Front-end/Optimus/src/app/cadastra-adm/cadastra-adm.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastra-adm',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastraADMComponent implements OnInit {
 
-  constructor() { }
+  user:Usuario = new Usuario
+  confirmarSenha: string
+  estadoUsuario: string
+  cargoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any){
+    this.confirmarSenha = event.target.value
+  }
+
+  selectEstado(event: any){
+    this.estadoUsuario = event.target.value
+  }
+
+  selectCargo(event: any){
+    this.cargoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.user.cargo = this.cargoUsuario
+    this.user.estado = this.estadoUsuario
+
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas')
+    }
+
+    else{
+      this.authService.cadastrar(this.user).subscribe((resp:Usuario)=>{
+        this.user=resp
+        this.router.navigate(['/home-adm'])
+        alert('Funcionario cadastrado com sucesso')
+      })
+    }
   }
 
 }
--- Front-end/Optimus/src/app/entra/entra.component.ts
+++ Front-end/Optimus/src/app/entra/entra.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entra',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntraComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(){
+  }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp:UserLogin) =>{
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.id = this.userLogin.id_usuario
+      environment.nome = this.userLogin.nome
+      environment.tipo = this.userLogin.tipo
+      environment.email = this.userLogin.email
+
+      this.router.navigate(['/produtoCliente'])
+    }, erro => {
+      if(erro.status == 500){
+        alert('Usuário ou senha estão incorretos')
+      }
+    })
   }
 
 }
